937169464a5a4dd8dd2332b8d0118f2b035b239e
Andrey Krutogolov <andrey.krutogolov@gmail.com>
2015-08-14 16:56:30 +0300

    minor

---
+++ src/algorithm/Test.java
@@ -0,0 +1,27 @@
+package algorithm;
+
+import java.util.concurrent.ForkJoinPool;
+import algorithm.Problem;
+import algorithm.Solver;
+
+public class Test {
+
+  public static void main(String[] args) {
+    Problem test = new Problem();
+    // check the number of available processors
+    int nThreads = Runtime.getRuntime().availableProcessors();
+    System.out.println(nThreads);
+    Solver solver = new Solver(test.getList());
+    ForkJoinPool pool = new ForkJoinPool();
+    pool.invoke(solver);
+    long result = solver.getResult();
+    System.out.println("Done. Result: " + result);
+    long sum = 0;
+    // check if the result was ok
+    for (int i = 0; i < test.getList().length; i++) {
+      sum += test.getList()[i];
+    }
+    pool.shutdown();
+    System.out.println("Done. Result: " + sum);
+  }
+} 
---
+++ src/test/futures/FuturesTest.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package test.futures;
+
+/**
+ * @author andy
+ *
+ */
+public class FuturesTest {
+
+  /**
+   * @param args
+   */
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+
+  }
+
+}
