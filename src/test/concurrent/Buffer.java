8b682a51cd4564a900749a1bd0d85d2471607f5a
Andrey Krutogolov <andrey.krutogolov@gmail.com>
2016-06-05 22:12:28 +0300

    minor

---
+++ src/test/concurrent/Buffer.java
@@ -0,0 +1,13 @@
+package test.concurrent;
+
+import java.util.logging.Logger;
+
+public class Buffer {
+  private static final Logger log = Logger.getLogger(Buffer.class.getName());
+
+  
+public static void main(String[] args) {
+
+}
+
+}
