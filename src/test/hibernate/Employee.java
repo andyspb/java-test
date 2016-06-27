803d16b4984b0c2034e8db5236f6c7798df554e7
Andrey Krutogolov <andrey.krutogolov@gmail.com>
2016-06-27 11:32:25 +0300

    hibernate

--- src/test/hibernate/Employee.java
+++ src/test/hibernate/Employee.java
@@ -1,9 +1,22 @@
 package test.hibernate;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Id;
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+
+@Entity
+@Table(name = "employee")
 public class Employee {
+  @Id @GeneratedValue
+  @Column(name = "id")
   private int id;
+  @Column(name = "first_name")
   private String firstName;
+  @Column(name = "last_name")
   private String lastName;
+  @Column(name = "salary")
   private int salary;
 
   public Employee() {}
