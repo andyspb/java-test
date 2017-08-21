6bf5986c544b24a8d03a0e560fad0395bf5e855e
Andrey Krutogolov <andrey.krutogolov@gmail.com>
2017-08-21 14:18:06 +0100

    New examples added

--- src/array/Test.java
+++ src/array/Test.java
@@ -3,9 +3,8 @@ package array;
 public class Test {
   public static void main(String args[]) {
     int arr[] = {10, 20, 30, 40, 50};
-    for(int i=0; i < arr.length; i++)
-    {
-          System.out.print(" " + arr[i]);
+    for (int i = 0; i < arr.length; i++) {
+      System.out.print(" " + arr[i]);
     }
- }
+  }
 }
--- src/assertion/AssertionExample.java
+++ src/assertion/AssertionExample.java
@@ -4,14 +4,14 @@ import java.util.Scanner;
 
 public class AssertionExample {
 
-  public static void main( String args[] ){
+  public static void main(String args[]) {
 
-    Scanner scanner = new Scanner( System.in );
+    Scanner scanner = new Scanner(System.in);
     System.out.print("Enter your age ");
 
     int value = scanner.nextInt();
-    assert value>=18:" Not valid";
+    assert value >= 18 : " Not valid";
 
-    System.out.println("value is "+value);
-   }
+    System.out.println("value is " + value);
+  }
 }
