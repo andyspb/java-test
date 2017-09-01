6e46507774ee199d33c700a2d7d5f39a2aea4465
Andrey Krutogolov <andrey.krutogolov@gmail.com>
2017-09-01 11:42:24 +0100

    New examples added

--- src/algorithm/MergeSort.java
+++ src/algorithm/MergeSort.java
@@ -4,9 +4,10 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class MergeSort {
+  private static final Logger log = Logger.getLogger(MergeSort.class.getName());
+
   private int[] numbers;
   private int[] helper;
-
   private int number;
 
   public void sort(int[] values) {
@@ -17,22 +18,15 @@ public class MergeSort {
   }
 
   private void mergesort(int low, int high) {
-    // check if low is smaller than high, if not then the array is sorted
     if (low < high) {
-      // Get the index of the element which is in the middle
       int middle = low + (high - low) / 2;
-      // Sort the left side of the array
       mergesort(low, middle);
-      // Sort the right side of the array
       mergesort(middle + 1, high);
-      // Combine them both
       merge(low, middle, high);
     }
   }
 
   private void merge(int low, int middle, int high) {
-
-    // Copy both parts into the helper array
     for (int i = low; i <= high; i++) {
       helper[i] = numbers[i];
     }
@@ -40,34 +34,24 @@ public class MergeSort {
     int i = low;
     int j = middle + 1;
     int k = low;
-    // Copy the smallest values from either the left or the right side back
-    // to the original array
     while (i <= middle && j <= high) {
       if (helper[i] <= helper[j]) {
         numbers[k] = helper[i];
-        i++;
+        ++i;
       } else {
         numbers[k] = helper[j];
-        j++;
+        ++j;
       }
-      k++;
+      ++k;
     }
-    // Copy the rest of the left side of the array into the target array
     while (i <= middle) {
       numbers[k] = helper[i];
       ++k;
       ++i;
     }
-    // Since we are sorting in-place any leftover elements from the right side
-    // are already at the right position.
-
   }
-
-  private static final Logger log = Logger.getLogger(MergeSort.class.getName());
 
   public static void main(String[] args) {
     log.log(Level.INFO, "From MergeSort");
-
   }
-
 }
