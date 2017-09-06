999b1aac151531126fb7fd5bbd3ae37eb82918d3
Andrey Krutogolov <andrey.krutogolov@gmail.com>
2017-09-06 16:39:58 +0100

    New examples added

--- src/multithreading/ThreeThreadsTest.java
+++ src/multithreading/ThreeThreadsTest.java
@@ -3,10 +3,11 @@ package multithreading;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-class MyThread implements Runnable {
+class MyThread extends Thread {
   private static final Logger log = Logger.getLogger(MyThread.class.getName());
 
   private String name;
+  @SuppressWarnings("unused")
   private MyThread thread;
 
   public MyThread(String name, MyThread t) {
@@ -16,9 +17,6 @@ class MyThread implements Runnable {
 
   @Override
   public void run() {
-    if (thread != null) {
-      thread.join();
-    }
     log.log(Level.INFO, "From thread: " + name);
   }
 
@@ -35,6 +33,20 @@ public class ThreeThreadsTest {
     MyThread t1 = new MyThread("t1", null);
     MyThread t2 = new MyThread("t2", t1);
     MyThread t3 = new MyThread("t3", t2);
+
+    t1.start();
+    t2.start();
+    t3.start();
+    
+    try {
+      t1.join();
+      t2.join();
+      t3.join();
+    } catch (InterruptedException e) {}
+    
+    
+    
+    log.log(Level.INFO, "<<<");
 
   }
 
