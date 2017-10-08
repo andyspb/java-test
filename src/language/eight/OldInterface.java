a9e9f7f0ccb57da4c54f9213ae6aaab66f7704f1
Andrey Krutogolov <andrey.krutogolov@gmail.com>
2017-10-09 00:15:16 +0300

    minor changes

---
+++ src/language/eight/OldInterface.java
@@ -0,0 +1,10 @@
+package language.eight;
+
+public interface OldInterface {
+  public void existingMethod();
+
+  default public void newDefaultMethod() {
+      System.out.println("New default method is added in interface");
+  }
+
+}
---
+++ src/language/eight/TimeClient.java
@@ -0,0 +1,26 @@
+package language.eight;
+
+import java.time.*;
+
+public interface TimeClient {
+  public void setTime(int hour, int minute, int second);
+
+  public void setDate(int day, int month, int year);
+
+  public void setDateAndTime(int day, int month, int year, int hour, int minute, int second);
+
+  public LocalDateTime getLocalDateTime();
+
+  static public ZoneId getZoneId(String zoneString) {
+    try {
+      return ZoneId.of(zoneString);
+    } catch (DateTimeException e) {
+      System.err.println("Invalid time zone: " + zoneString + "; using default time zone instead.");
+      return ZoneId.systemDefault();
+    }
+  }
+
+  public default ZonedDateTime getZonedDateTime(String zoneString) {
+    return ZonedDateTime.of(getLocalDateTime(), getZoneId(zoneString));
+  }
+}
