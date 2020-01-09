56ea27407ae92a542f61e7d601cfefa997cfaca9
Andrey Krutogolov <andrey.krutogolov@gmail.com>
2020-01-09 16:26:34 +0300

    Fix

---
+++ src/main/java/com/javatest/annotation/MyAnnotationRunner.java
@@ -0,0 +1,7 @@
+package com.javatest.annotation;
+
+public class MyAnnotationRunner  {
+
+    public MyAnnotationRunner(Class<?> klass){
+    }
+}
---
+++ src/main/java/com/javatest/annotation/TestExample.java
@@ -0,0 +1,8 @@
+package com.javatest.annotation;
+
+public class TestExample {
+  @Test
+  public void TestInt() {
+    System.out.println("TestInt");
+  }
+}
