8566fb029f2703b74893483353e6e11ffa76940a
Andrey Krutogolov <andrey.krutogolov@gmail.com>
2020-01-24 15:45:26 +0300

    Minor refactoring

--- scala-test/src/Collections.scala
+++ scala-test/src/Collections.scala
@@ -8,13 +8,14 @@ object Collections {
   val list = List();
 
   def main(args: Array[String]) {
-    val i: Int = 0
+    var i: Int = 0
 
     def ++ = i + 1
 
     while (i < 10) {
       list :+ i
-      //      i = i+1
+      println(i)
+      i = i +1
     }
 
   }
