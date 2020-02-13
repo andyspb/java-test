f60f249c5d582dd7b8be927acbf6856612802187
Andrey Krutogolov <andrey.krutogolov@gmail.com>
2020-02-13 23:22:32 +0300

    Added spring-webflow

--- java-tests/src/test/java/com/test/AtomicExampleTest.java
+++ java-tests/src/test/java/com/test/AtomicExampleTest.java
@@ -2,22 +2,37 @@ package com.test;
 
 import com.test.atomic.Book;
 import com.test.atomic.MyObject;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.*;
 
 /** @author Andrey Krutogolov */
 public class AtomicExampleTest {
   private MyObject myObject;
 
+  @BeforeClass
+  public static void runOnceBeforeClass() {
+    System.out.println("@BeforeClass - runOnceBeforeClass");
+  }
+
+  @AfterClass
+  public static void runOnceAfterClass() {
+    System.out.println("@AfterClass - runOnceAfterClass");
+  }
+
   @Before
   public void setUp() {
+    System.out.println("@Before - runBeforeTestMethod");
     myObject = new MyObject();
     myObject.setWhatImReading(new Book("Java 2 From Scratch"));
   }
 
+  @After
+  public void runAfterTestMethod() {
+    System.out.println("@After - runAfterTestMethod");
+  }
+
   @Test
   public void testUpdate() {
+    System.out.println("@Test -testMethod");
     myObject.setWhatImReading(new Book("Pro Java EE 5 Performance Management and Optimization"));
     Assert.assertEquals(
         "Incorrect book name",
