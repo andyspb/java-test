332db3f07d99bdaee24dde952cadbdb9e6c6f77a
Andrey Krutogolov <andrey.krutogolov@gmail.com>
2020-06-17 08:11:27 +0300

    Added new LC examples

--- java-tests/src/main/java/com/leet/SubstringWithConcatenationWords.java
+++ java-tests/src/main/java/com/leet/SubstringWithConcatenationWords.java
@@ -2,9 +2,9 @@ package com.leet;
 
 import java.util.List;
 
-// You are given a string, s, and a list of words, words,
-// that are all of the same length. Find all starting indices of substring(s) in s that is a
-// concatenation of each word in words exactly once and without any intervening characters.
+// You are given a string, s, and a list of words, words, that are all of the same length.
+// Find all starting indices of substring(s) in s that is a concatenation of each word in words
+// exactly once and without any intervening characters.
 
 public class SubstringWithConcatenationWords {
   public List<Integer> findSubstring(String s, String[] words) {
--- java-tests/src/main/java/com/leet/array/ValidSudoku.java
+++ java-tests/src/main/java/com/leet/array/ValidSudoku.java
@@ -1,6 +1,12 @@
 package com.leet.array;
 
 public class ValidSudoku {
+  //  Determine if a 9x9 Sudoku board is valid. Only the filled cells need to be validated according
+  // to the following rules:
+  //  Each row must contain the digits 1-9 without repetition.
+  //  Each column must contain the digits 1-9 without repetition.
+  //  Each of the 9 3x3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+
   public boolean isValidSudoku(char[][] board) {
     if (board == null || board.length != 9 || board[0].length != 9) return false;
     return true;
