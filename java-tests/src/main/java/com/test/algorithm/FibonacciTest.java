3b962a97b7780315c1d64113f8c9a948a04c12b9
Andrey Krutogolov <andrey.krutogolov@gmail.com>
2021-04-02 12:55:18 -0400

    Added new LUX example

--- java-tests/src/main/java/com/test/algorithm/FibonacciTest.java
+++ java-tests/src/main/java/com/test/algorithm/FibonacciTest.java
@@ -9,6 +9,21 @@ public class FibonacciTest {
     return fib(n - 1) + fib(n - 2);
   }
 
+  public int fibonacciIterative(int n) {
+    if(n <= 1) {
+      return n;
+    }
+    int fib = 1;
+    int prevFib = 1;
+
+    for(int i=2; i<n; i++) {
+      int temp = fib;
+      fib+= prevFib;
+      prevFib = temp;
+    }
+    return fib;
+  }
+
   public static void main(String[] args) {
     System.out.println("Fibonacci test:");
 
